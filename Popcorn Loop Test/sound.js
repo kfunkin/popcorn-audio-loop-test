317495bff354a288224a08c412eb674560cd2da4
kfunkin <32571927+kfunkin@users.noreply.github.com>
2018-05-01 15:55:14 +0100

    Add files via upload

---
+++ Popcorn Loop Test/sound.js
@@ -0,0 +1,13 @@
+
+var myAudio = document.getElementById("drums");
+
+function togglePlay() {
+  return drums.paused ? drums.play() : drums.pause();
+};
+
+
+var myAudio = document.getElementById("synth");
+
+function togglePlay() {
+  return synth.paused ? synth.play() : synth.pause();
+};
---
+++ Popcorn Loop Test/sound2.js
@@ -0,0 +1,5 @@
+var myAudio = document.getElementById("synth");
+
+  function togglePlay() {
+      return synth.paused ? synth.play() : synth.pause();
+    };
